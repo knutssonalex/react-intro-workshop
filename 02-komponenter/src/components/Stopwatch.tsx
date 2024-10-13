569ce588ffe2976d8061c22a86a60bbcfcfbd5ee
Alex Knutsson <knutssonalex@gmail.com>
2024-10-13 10:19:51 +0200

    Add 02-komponenter

---
+++ 02-komponenter/src/components/Stopwatch.tsx
@@ -0,0 +1,11 @@
+const Stopwatch = () => {
+  return (
+    <div className="container">
+      <div className="box">
+        <h1>Stopwatch</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Stopwatch
