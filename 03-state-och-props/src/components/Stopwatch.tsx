c173597b93db4481b2d54824d34fdc1e33af533d
Alex Knutsson <knutssonalex@gmail.com>
2024-10-14 18:59:01 +0200

    Refactor Stopwatch component to display time in HH:MM:SS format

--- 03-state-och-props/src/components/Stopwatch.tsx
+++ 03-state-och-props/src/components/Stopwatch.tsx
@@ -1,6 +1,15 @@
 import { useRef, useState } from 'react'
 import ControlButtons from './ControlButtons'
 
+const formatTime = (time: number) => {
+  const milliseconds = `00${time % 1000}`.slice(-3)
+  const seconds = `0${Math.floor(time / 1000) % 60}`.slice(-2)
+  const minutes = `0${Math.floor(time / 60000) % 60}`.slice(-2)
+  const hours = `0${Math.floor(time / 3600000)}`.slice(-2)
+
+  return `${hours}:${minutes}:${seconds}.${milliseconds}`
+}
+
 const Stopwatch = () => {
   const [time, setTime] = useState<number>(0)
   const [isRunning, setIsRunning] = useState<boolean>(false)
@@ -33,7 +42,7 @@ const Stopwatch = () => {
     <div className="container">
       <div className="box">
         <h1>Stopwatch</h1>
-        <div className="stopwatch-display">{time}</div>
+        <div className="stopwatch-display">{formatTime(time)}</div>
         <ControlButtons
           isRunning={isRunning}
           onStart={startStopwatch}
